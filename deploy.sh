1fc8890a89c96cb3ca209f2890e0258c63a03a1f
JIAYU_WANG <JIAYU_WANG@trtjk.com>
2019-01-30 16:40:39 +0800

    修改deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -l
-DONTKILLME
 export PROJ_PATH=`pwd`
 export TOMCAT_PATH=/usr/local/tomcat/apache-tomcat-9.0.14
-cd $PROJ_PATH/
 jar -cvf ROOT.war ./*
 cp $PROJ_PATH/ROOT.war $TOMCAT_PATH/webapps/
-cd $TOMCAT_PATH/webapps/
