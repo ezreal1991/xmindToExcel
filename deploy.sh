3c4ec3f19e87e44a684e6ebe11ec014b89aa3893
JIAYU_WANG <JIAYU_WANG@trtjk.com>
2019-01-31 09:25:54 +0800

    修改deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -l
 BUILD_ID=DONTKILLME
 . /etc/profile
-export PATH=$PATH:$HOME/bin:/sbin:/usr/bin:/usr/sbin
 export PROJ_PATH=`pwd`
 export TOMCAT_PATH=/usr/local/tomcat/apache-tomcat-9.0.14
 jar -cvf ROOT.war ./*
-docker restart tomcat2 
+docker restart tomcat2
