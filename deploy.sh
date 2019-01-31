b2f44f0430bea7c8fa252cf0913ce35117ab6700
JIAYU_WANG <JIAYU_WANG@trtjk.com>
2019-01-31 09:24:29 +0800

    修改deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ export PATH=$PATH:$HOME/bin:/sbin:/usr/bin:/usr/sbin
 export PROJ_PATH=`pwd`
 export TOMCAT_PATH=/usr/local/tomcat/apache-tomcat-9.0.14
 jar -cvf ROOT.war ./*
-cp $PROJ_PATH/ROOT.war $TOMCAT_PATH/webapps/
+docker restart tomcat2 
